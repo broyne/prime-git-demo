c813068b632fb1d65f1f05c1b5fa2ed7a3da0b88
yatrihirani <hiraniyatri@gmail.com>
2024-07-21 00:29:35 +1000

    add name in different line

--- src/main/java/com/prime/PrintMyName.java
+++ src/main/java/com/prime/PrintMyName.java
@@ -2,6 +2,7 @@ package com.prime;
 
 public class PrintMyName {
     public static void main(String[] args) {
-        System.out.println("Yatri Hirani");
+        System.out.println("Hirani");
+        System.out.println("YAtri");
     }
 }
